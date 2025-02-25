130aeed751cadbdbc29a89d8cb7d7233e8a9fb56
shubham-2728 <91383808+shubham-2728@users.noreply.github.com>
2025-02-25 10:45:18 +0530

    Update testMongo.js

--- testMongo.js
+++ testMongo.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-DATABASE_URL = "mongodb+srv://Shubham:ReE1g8NxASuf8vn3@nextcluster.1rb0f.mongodb.net/NextJSApp"
+DATABASE_URL = " " //Enter database URL
 const uri = process.env.DATABASE_URL; // Using the .env value
 const client = new MongoClient(uri);
 
